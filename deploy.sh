1cddaf458d09966d43b45fe42c76845cd362b66b
shezana <shezana@localhost.localdomain>
2024-12-19 05:59:54 -0500

    Added execute permissions to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ docker stop ci-cd-pipeline-container
 docker ci-cd-pipeline-container
 
 # Run a new container
-docker run -d --name ci-cd-pipeline-container -p 80:80 shezana/ci-cd-pipeline
+docker run -d --name ci-cd-pipeline-container -p 8081:80 shezana/ci-cd-pipeline
 
