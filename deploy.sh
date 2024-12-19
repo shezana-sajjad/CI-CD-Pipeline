9bb11bef7b6f3b3c9bf1d189b68bb3f700c1ab83
shezana <shezana@localhost.localdomain>
2024-12-19 06:02:20 -0500

    Added execute permissions to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ docker pull shezana/ci-cd-pipeline
 
 # Stop and remove the previous container
 docker stop ci-cd-pipeline-container
-docker ci-cd-pipeline-container
+docker rm ci-cd-pipeline-container
 
 # Run a new container
 docker run -d --name ci-cd-pipeline-container -p 8081:80 shezana/ci-cd-pipeline
